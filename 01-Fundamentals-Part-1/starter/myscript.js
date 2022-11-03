a873705935e717ae50aedffd8d58524bc7772253
jpg2022 <jpg22@att.net>
2022-11-03 18:12:00 -0500

    2nd coding challenge

--- 01-Fundamentals-Part-1/starter/myscript.js
+++ 01-Fundamentals-Part-1/starter/myscript.js
@@ -1,1 +1,46 @@
-console.log("Hello World!")
+/*
+Code challenge 1
+
+let markweight = 95
+let markheight = 1.88
+let johnweight = 85
+let johnheight = 1.76
+
+let johnbmi = johnweight / (johnheight **2)
+let markbmi = markweight / (markheight ** 2)
+
+console.log(markbmi> johnbmi)
+
+*/
+
+//code challange 2
+/*
+let markweight = 95
+let markheight = 1.88
+let johnweight = 85
+let johnheight = 1.76
+
+let johnbmi = johnweight / (johnheight **2)
+let markbmi = markweight / (markheight ** 2)
+let greater = '';
+let greaterbmi = '';
+let lesser = '';
+let lesserbmi = '';
+if(markbmi > johnbmi){
+    greater = "Mark"
+    greaterbmi = markbmi
+    lesser = "John"
+    lesserbmi = johnbmi
+
+}else{
+    greater = "John"
+    greaterbmi = johnbmi
+    lesser = "Mark"
+    lesserbmi = markbmi
+}
+
+let out = `${greater}s  BMI (${greaterbmi}) is higher than ${lesser}s `+`(${lesserbmi}!`
+console.log(out)
+
+*/
+
